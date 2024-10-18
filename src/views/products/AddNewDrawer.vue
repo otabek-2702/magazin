cbe67a2579cb97c34650f94386ed36745287d3e5
otabek-2702 <adrian3806880@gmail.com>
2024-10-18 13:44:57 +0500

    product variant adddrawer update

--- src/views/products/AddNewDrawer.vue
+++ src/views/products/AddNewDrawer.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { PerfectScrollbar } from 'vue3-perfect-scrollbar';
-import { nextTick, ref, watchEffect } from 'vue';
-import AppDrawerHeaderSection from '@core/components/AppDrawerHeaderSection.vue';
-import axios from '@axios';
-import { toast } from 'vue3-toastify';
+import { PerfectScrollbar } from "vue3-perfect-scrollbar";
+import { nextTick, ref, watchEffect } from "vue";
+import AppDrawerHeaderSection from "@core/components/AppDrawerHeaderSection.vue";
+import axios from "@axios";
+import { toast } from "vue3-toastify";
 
 const props = defineProps({
   isDrawerOpen: {
@@ -12,7 +12,7 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['update:isDrawerOpen', 'fetchDatas']);
+const emit = defineEmits(["update:isDrawerOpen", "fetchDatas"]);
 
 const isFetching = ref(false);
 const isFormValid = ref(false);
@@ -21,19 +21,21 @@ const name = ref();
 const brand = ref();
 const supplier_id = ref();
 const category_id = ref();
-const season = ref('fall');
-const gender = ref('man');
+const season = ref("fall");
+const gender = ref("man");
+const colors_id = ref();
+const sizes_id = ref();
 const profit_percent = ref(100);
 const sale = ref(0);
 
 // 👉 drawer close
 const closeNavigationDrawer = () => {
-  emit('update:isDrawerOpen', false);
+  emit("update:isDrawerOpen", false);
   nextTick(() => {
     refForm.value?.reset();
     refForm.value?.resetValidation();
-    season.value = 'fall';
-    gender.value = 'man';
+    season.value = "fall";
+    gender.value = "man";
     profit_percent.value = 100;
     sale.value = 0;
   });
@@ -44,7 +46,7 @@ const onSubmit = () => {
     if (valid) {
       isFetching.value = true;
       try {
-        await axios.post('/products', {
+        await axios.post("/products", {
           name: name.value,
           brand: brand.value,
           supplier_id: supplier_id.value,
@@ -53,11 +55,13 @@ const onSubmit = () => {
           gender: gender.value,
           profit_percent: profit_percent.value,
           sale: sale.value,
+          sizes_list: sizes_id.value,
+          colors_list: colors_id.value,
         });
-        emit('fetchDatas');
-        toast('Успешно добавлено', {
-          theme: 'auto',
-          type: 'success',
+        emit("fetchDatas");
+        toast("Успешно добавлено", {
+          theme: "auto",
+          type: "success",
           dangerouslyHTMLString: true,
         });
         handleDrawerModelValueUpdate(false);
@@ -71,7 +75,7 @@ const onSubmit = () => {
 };
 
 const handleDrawerModelValueUpdate = (val) => {
-  emit('update:isDrawerOpen', val);
+  emit("update:isDrawerOpen", val);
   if (!val) {
     nextTick(() => {
       refForm.value?.reset();
@@ -85,8 +89,12 @@ const suppliers_list = ref([]);
 const colors_list = ref([]);
 const sizes_list = ref([]);
 
-
-const fetchOptions = async (url, dataState, key, customization = { is: false }) => {
+const fetchOptions = async (
+  url,
+  dataState,
+  key,
+  customization = { is: false }
+) => {
   try {
     const response = await axios.get(url);
 
@@ -105,12 +113,14 @@ const fetchOptions = async (url, dataState, key, customization = { is: false }) 
 watch(
   () => props.isDrawerOpen,
   () => {
-    fetchOptions('/categories', categories_list, 'categories')
-    fetchOptions('/suppliers', suppliers_list, 'suppliers')
-    fetchOptions('/sizes', sizes_list, 'sizes')
-    fetchOptions('/colors', colors_list, 'colors')
+    if (props.isDrawerOpen) {
+      fetchOptions("/categories", categories_list, "categories");
+      fetchOptions("/suppliers", suppliers_list, "suppliers");
+      fetchOptions("/sizes", sizes_list, "sizes");
+      fetchOptions("/colors", colors_list, "colors");
+    }
   },
-  { once: true },
+  { once: true }
 );
 </script>
 
@@ -130,7 +140,7 @@ watch(
       <VCard flat>
         <VCardText>
           <!-- 👉 Форма -->
-          <VForm ref="refForm" v-model="isFormValid" @submit.prevent="onSubmit" >
+          <VForm ref="refForm" v-model="isFormValid" @submit.prevent="onSubmit">
             <VRow>
               <!-- 👉 Полное имя -->
               <VCol cols="12">
@@ -163,10 +173,30 @@ watch(
 
               <VCol cols="12">
                 <VRadioGroup v-model="season" inline>
-                  <VRadio label="Весна" value="spring" density="compact" color="success" />
-                  <VRadio label="Лето" value="summer" density="compact" color="#FFEB3B" />
-                  <VRadio label="Осень" value="fall" density="compact" color="#FF9800" />
-                  <VRadio label="Зима" value="winter" density="compact" color="#03A9F4" />
+                  <VRadio
+                    label="Весна"
+                    value="spring"
+                    density="compact"
+                    color="success"
+                  />
+                  <VRadio
+                    label="Лето"
+                    value="summer"
+                    density="compact"
+                    color="#FFEB3B"
+                  />
+                  <VRadio
+                    label="Осень"
+                    value="fall"
+                    density="compact"
+                    color="#FF9800"
+                  />
+                  <VRadio
+                    label="Зима"
+                    value="winter"
+                    density="compact"
+                    color="#03A9F4"
+                  />
                 </VRadioGroup>
               </VCol>
 
@@ -174,25 +204,68 @@ watch(
                 <VRadioGroup v-model="gender" inline>
                   <VRadio label="Мужской" value="man" density="compact" />
                   <VRadio label="Женский" value="woman" density="compact" />
-                  <VRadio label="Универсальный" value="unisex" density="compact" />
+                  <VRadio
+                    label="Универсальный"
+                    value="unisex"
+                    density="compact"
+                  />
                   <VRadio label="Детский" value="kids" density="compact" />
                 </VRadioGroup>
               </VCol>
 
               <VCol cols="12">
-                <VTextField v-model="profit_percent" label="Наценка в процентах" type="number" />
+                <VAutocomplete
+                  v-model="colors_id"
+                  label="Выберите цвет"
+                  :items="colors_list"
+                  item-title="name"
+                  item-value="id"
+                  multiple
+                />
+              </VCol>
+              <VCol cols="12">
+                <VSelect
+                  v-model="sizes_id"
+                  label="Выберите размер"
+                  :items="sizes_list"
+                  item-title="name"
+                  item-value="id"
+                  multiple
+                />
               </VCol>
 
               <VCol cols="12">
-                <VTextField v-model="sale" label="Скидка в процентах" type="number" />
+                <VTextField
+                  v-model="profit_percent"
+                  label="Наценка в процентах"
+                  type="number"
+                />
+              </VCol>
+
+              <VCol cols="12">
+                <VTextField
+                  v-model="sale"
+                  label="Скидка в процентах"
+                  type="number"
+                />
               </VCol>
 
               <!-- 👉 Отправить и Отмена -->
               <VCol cols="12">
-                <VBtn :loading="isFetching" :disabled="isFetching" type="submit" class="me-3">
+                <VBtn
+                  :loading="isFetching"
+                  :disabled="isFetching"
+                  type="submit"
+                  class="me-3"
+                >
                   Отправить
                 </VBtn>
-                <VBtn type="reset" variant="tonal" color="secondary" @click="closeNavigationDrawer">
+                <VBtn
+                  type="reset"
+                  variant="tonal"
+                  color="secondary"
+                  @click="closeNavigationDrawer"
+                >
                   Отмена
                 </VBtn>
               </VCol>
