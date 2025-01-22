47de31f23315c9563af6fae746fb2038310ff8be
otabek-2702 <adrian3806880@gmail.com>
2025-01-22 13:33:41 +0500

    go

--- src/views/cash-register/ConfirmDialog.vue
+++ src/views/cash-register/ConfirmDialog.vue
@@ -43,16 +43,24 @@ const onConfirm = async () => {
 
   isFetching.value = true;
   try {
+    const clear_phone = phone_number.value?.replace(/\s+/g, "");
+
+    let params = {
+      payments: multi_prices.value.map((el) => ({
+        type_id: el.type_id,
+        price: removeSpaces(el.price),
+      })),
+      sale: removeSpaces(sale_price.value),
+    
+    };
+
+    if (clear_phone) {
+      params.phone = "+998" + clear_phone;
+    }
+
     const response = await axios.post(
       `/payment_invoices/confirm/${props.paymentInvoice?.id}`,
-      {
-        payments: multi_prices.value.map((el) => ({
-          type_id: el.type_id,
-          price: removeSpaces(el.price),
-        })),
-        sale: removeSpaces(sale_price.value),
-        phone_number: removeSpaces(phone_number.value),
-      }
+      params
     );
 
     if (response.status === 200) {
